c0e16746e721f2f2fa6725222d3d94c91124eded
PeicongZhan <972260627@qq.com>
2020-11-21 00:52:11 +0800

    Modify the data with CSV,create 3 headings and table to show the info of players,create the heroes route to show the list of palyers.

--- app/Controllers/Playersid.php
+++ app/Controllers/Playersid.php
@@ -1,5 +1,6 @@
 <?php
  namespace App\Controllers;
+
  class Playersid extends BaseController
  {
  public function index()
@@ -7,14 +8,40 @@
      // connect to the model
  $places = new \App\Models\Playersinfo();
  // retrieve all the records
- $records = $places->findAll();
+     $records = $places->findAll();
+ 
+  $table = new \CodeIgniter\View\Table();
+        $headings = $places->fields;
+        $displayHeadings = array_slice($headings, 1,2);
+        $table->setHeading(array_map('ucfirst', $displayHeadings));
+        foreach ($records as $record) {
+            $nameLink = anchor("playersid/showme/$record->id");
+            $table->addRow($record->name,"<img src=\"/image/".$record->image."\"/>",$nameLink);
+        }
+
+        $template = [
+            'table_open' => '<table cellpadding="5px">',
+            'cell_start' => '<td style="border: 1px solid #dddddd;">',
+            'row_alt_start' => '<tr style="background-color:#dddddd">',
+            
+        ];
+        $fields = [
+            'title' => 'Lakers championship!!!',
+            'heading' => 'Six core members',
+            'footer' => 'Copyright Peicong Zhan'
+        ];
+        $table->setTemplate($template);
+
+        
+ 
  
  // get a template parser
  $parser = \Config\Services::parser();
- // tell it about the substitions
- return $parser->setData(['records' => $records])
- // and have it render the template with those
- ->render('memberlist');
+        return $parser->setData($fields)
+                ->render('templates\top') .
+                $table->generate() .
+                $parser->setData($fields)
+                ->render('templates\bottom');
  }
  
  public function showme($id)
@@ -24,11 +51,40 @@
  // retrieve all the records
  $record = $places->find($id);
  
+    $table = new \CodeIgniter\View\Table();
+        $headings = $places->fields;
+        $table->addRow($headings[0], $record['id'])
+                ->addRow($headings[1], $record['name'])
+                ->addRow($headings[2], "<img src=\"/image/".$record['image']."\"/>")
+                ->addRow($headings[3], $record['birth'])
+                ->addRow($headings[4], $record['height'])
+                ->addRow($headings[5], $record['weight'])
+                ->addRow($headings[6], $record['nationality'])
+                ->addRow($headings[7], $record['team'])
+                ->addRow($headings[8], $record['achievements']);
+        
+               
+
+        $template = [
+            'table_open' => '<table cellpadding="5px">',
+            'cell_start' => '<td style="border: 1px solid #dddddd;">',
+            'row_alt_start' => '<tr style="background-color:#dddddd">',
+        ];
+        
+
+        $fields = [
+            'title' => 'Lakers !',
+            'heading' => 'player',
+            'footer' => 'Copyright Peicong Zhan'
+        ];
+        $table->setTemplate($template);
+ 
  // get a template parser
  $parser = \Config\Services::parser();
- // tell it about the substitions
- return $parser->setData($record)
- // and have it render the template with those
- ->render('memberid');
+        return $parser->setData($fields)
+                ->render('templates\top') .
+                $table->generate() .
+                $parser->setData($fields)
+                ->render('templates\bottom');
  }
  }
